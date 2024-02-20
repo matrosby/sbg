dfbfa02de8e7289800c64e3343c6a8be5be3186c
MATPOC <matros@tut.by>
2024-02-21 02:26:17 +0300

    Add files via upload

--- nicko.js
+++ nicko.js
@@ -49,7 +49,7 @@
 	const DISCOVERY_COOLDOWN = 90;
 	const HIGHLEVEL_MARKER = 9;
 	const HIT_TOLERANCE = 15;
-	const HOME_DIR = 'https://nicko-v.github.io/sbg-cui';
+	const HOME_DIR = 'https://matros.by/sbgn';
 	const INVENTORY_LIMIT = 3000;
 	const INVIEW_MARKERS_MAX_ZOOM = 16;
 	const INVIEW_POINTS_DATA_TTL = 7000;
@@ -1047,7 +1047,7 @@
 			}
 
 			async function getHTMLasset(filename) {
-				const url = `https://matros.by/sbgn/html/${filename}.html`;
+				const url = `${HOME_DIR}/html/${filename}.html`;
 				const response = await fetch(url);
 
 				if (response.status != 200) { throw new Error(`Ошибка при загрузке ресурса "${filename}.html" (${response.status})`); }
