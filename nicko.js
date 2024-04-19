761c13b1e9253e4229a745715ea8a694edbb7613
MATPOC <matros@tut.by>
2024-04-19 17:19:43 +0300

    Add files via upload

--- nicko.js
+++ nicko.js
@@ -1532,7 +1532,9 @@
 												drawButton.removeAttribute('sbgcui-possible-lines');
 												if (lastOpenedPoint.team == player.team) {
 													getPossibleLines(lastOpenedPoint.guid, lastOpenedPoint.coords).then(lines => {
-														if (lines.length > 0) { drawButton.setAttribute('sbgcui-possible-lines', lines.length); }
+														if (lines.length > 0) { drawButton.setAttribute('sbgcui-possible-lines', lines.length); } else {
+			drawButton.setAttribute('sbgcui-nopossible-lines', lines.length); 
+												}
 													});
 												}
 
