11c4942e7cbddabb265955d283cc1c44a4410288
MATPOC <matros@tut.by>
2024-11-28 20:28:03 +0300

    Add files via upload

--- nicko.js
+++ nicko.js
@@ -5661,7 +5661,7 @@
 							};
 							if (onClick == 'jumpto') {
 								toast.options.close = true;
-								toast.options.onClick = () => { toast.hideToast(); jumpTo(coords); };
+								toast.options.onClick = () => { toast.hideToast(); jumpTo(coords); highlightFeature(undefined, coords); };
 							}
 							destroyNotifsToasts.add(toast);
 
